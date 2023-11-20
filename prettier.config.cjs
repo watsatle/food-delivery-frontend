96c876dceba3759ea3049c94e3218de7edc9796d
tle <nantuko_titan@hotmail.com>
2023-11-20 10:18:00 +0700

    ci: :zap: ci/tle/add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+const config = {
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        options: {
+          tabWidth: 4,
+          semi: true,
+          parser: "typescript",
+        },
+      },
+      {
+        files: [
+          "**/*.cjs",
+          "**/*.js",
+          "**/*.mjs",
+          "**/*.yaml",
+          "**/*.yml",
+          "**/*.json",
+        ],
+        options: {
+          tabWidth: 2,
+          useTabs: false,
+        },
+      },
+    ],
+  };
+  
+  module.exports = config;
